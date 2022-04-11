e8ecffc78edd7af3d06267936b8a2bb5c1010e48
teristam <teristam@gmail.com>
2022-04-11 19:40:43 +0100

    feat: add setup.py options to compile cython funtion

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from distutils.core import setup
+from Cython.Build import cythonize
+import numpy
 
 setup(
     name='pyneurode',
@@ -6,4 +8,6 @@ setup(
     packages=['pyneurode',],
     author='Teris Tam',
     long_description=open('README.md').read(),
+    ext_modules=cythonize('pyneurode/spike_sorter_cy.pyx'),
+    include_dirs=[numpy.get_include()]
 )
