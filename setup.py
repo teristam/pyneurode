0fa0d57d1653168aebdf36fc87978191e25c3e0e
teristam <teristam@gmail.com>
2022-05-06 16:46:12 +0100

    git: ignore cython build files packaging: add build dependency in pyproject.toml, specify package requirements in setup.py instead of requirements

--- setup.py
+++ setup.py
@@ -9,5 +9,26 @@ setup(
     author='Teris Tam',
     long_description=open('README.md').read(),
     ext_modules=cythonize('pyneurode/spike_sorter_cy.pyx'),
-    include_dirs=[numpy.get_include()]
+    include_dirs=[numpy.get_include()], #for cythons
+    setup_requries=[
+        'cython',
+        'setuptools>=18.0',
+    ],
+    install_requires=[
+        'matplotlib',
+        'dearpygui == 1.3.1',
+        'scikit-learn',
+        'seaborn',
+        'numpy',
+        'shortuuid',
+        'networkx',
+        'pyzmq',
+        'jupyterlab',
+        'scipy[all]',
+        'Cython',
+        'Sphinx',
+        'myst-parser',
+        'palettable',
+        'tqdm',
+    ]
 )
