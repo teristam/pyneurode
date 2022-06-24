70382912ad89fb722da694b71d66f8e7e2b6e67e
Teris Tam <teristam@gmail.com>
2022-06-24 14:38:10 +0100

    change: add recording node back to the script

--- sorter_node_decode.py
+++ sorter_node_decode.py
@@ -1,9 +1,10 @@
 from pyneurode.processor_node.ArduinoSink import ArduinoSink
+from pyneurode.processor_node.ArduinoTriggerSink import ArduinoTriggerSink
 from pyneurode.processor_node.GUIProcessor import GUIProcessor
 from pyneurode.processor_node.Processor import *
 from pyneurode.processor_node.ProcessorContext import ProcessorContext
 from pyneurode.processor_node.FileReaderSource import FileReaderSource
-from pyneurode.processor_node.Spike2ArduinoProcessor import Spike2ArduinoProcessor
+from pyneurode.processor_node.Spike2ArduinoTriggerProcessor import Spike2ArduinoTriggerProcessor
 from pyneurode.processor_node.SpikeSortProcessor import SpikeSortProcessor
 from pyneurode.processor_node.SyncDataProcessor import SyncDataProcessor
 import pyneurode as dc
@@ -35,20 +36,24 @@ if  __name__ == '__main__':
         #TODO: need some way to query the output type of processor easily
 
         # reading too fast may overflow the pipe
-        # fileReader = FileReaderSource('../pyneurode_study/data/data_packets_M2_D23.pkl',interval=0.05)
-        zmqSource = ZmqSource(adc_channel=20)
+        # zmqSource = FileEchoSource(interval=0.01, filename='E:/decoder_test_data/M10_2022-06-23_12-43-50_test4/M10_test4_20220623_124341_packets.pkl', 
+        #                         filetype='message',batch_size=3)
+        zmqSource = ZmqSource(adc_channel=20,time_bin = 0.01)
         spikeSortProcessor = SpikeSortProcessor(interval=0.002, min_num_spikes=2000,time_bin=0.01)
         syncDataProcessor = SyncDataProcessor(interval=0.02)
-        gui = GUIProcessor()
+        gui = GUIProcessor(internal_buffer_size=5000)
+        spike2arduino = Spike2ArduinoTriggerProcessor([0,1,2,3,4,5], [13,12,11,10,9,8])
+        arduinoSink = ArduinoTriggerSink("COM4")
+        #potential bug: when it takes too long to acquire the spikes, the syncdata buffer may overflow
+
         animal_name = input('Please enter the designation of the animal: ')
         now = datetime.now()
         filesave = FileEchoSink(f'data/{animal_name}_{now.strftime("%Y%m%d_%H%M%S")}_df_sort_.pkl')
         packet_save = FileEchoSink(f'data/{animal_name}_{now.strftime("%Y%m%d_%H%M%S")}_packets.pkl')
-        spike2arduino = Spike2ArduinoProcessor([0,1,2,3,4,5], [13,12,11,10,9,8], 0.002)
-        arduinoSink = ArduinoSink("COM4")
 
         zmqSource.connect(spikeSortProcessor, filters='spike')
         zmqSource.connect(packet_save)
+
         spikeSortProcessor.connect(syncDataProcessor)
         spikeSortProcessor.connect(spike2arduino)
         spike2arduino.connect(arduinoSink)
@@ -60,9 +65,9 @@ if  __name__ == '__main__':
         spikeSortProcessor.connect(gui, ['df_sort','metrics'])
 
 
-        analog_visualizer = AnalogVisualizer('Synchronized signals',scale=20, buffer_length=1000)
-        pos_visualizer = AnalogVisualizer('pos', buffer_length=2000)
-        cluster_vis = SpikeClusterVisualizer('cluster_vis', max_spikes=200)
+        analog_visualizer = AnalogVisualizer('Synchronized signals',scale=20, buffer_length=6000)
+        pos_visualizer = AnalogVisualizer('pos', buffer_length=6000)
+        cluster_vis = SpikeClusterVisualizer('cluster_vis')
         latency_vis = LatencyVisualizer('latency')
 
 
@@ -71,6 +76,6 @@ if  __name__ == '__main__':
         gui.register_visualizer(cluster_vis, filters=['df_sort'])
         gui.register_visualizer(latency_vis, filters=['metrics'])
 
-        ctx.register_processors(zmqSource, spikeSortProcessor, syncDataProcessor, gui, filesave, arduinoSink, spike2arduino, packet_save)
+        ctx.register_processors(zmqSource, spikeSortProcessor, syncDataProcessor, gui, arduinoSink, spike2arduino, packet_save, filesave)
         
         ctx.start()
