c1bd71f70d8c6e620347d762287f9fa72854714a
teristam <teristam@gmail.com>
2022-06-26 23:45:59 +0100

    arduino_node: change to send trigger on every spike bin instead of the whole spiketrain

--- src/pyneurode/processor_node/Spike2ArduinoTriggerProcessor.py
+++ src/pyneurode/processor_node/Spike2ArduinoTriggerProcessor.py
@@ -10,7 +10,7 @@ class Spike2ArduinoTriggerProcessor(Processor):
     """Convert the spike of a neuron to AdruinoMessage so that it can be used to trigger external device
     """
     
-    def __init__(self, neuron_idx:list, digital_port:list):
+    def __init__(self, neuron_idx:list, digital_port:list, max_frame_to_skip=100):
         super().__init__()
         
         """Constructor
@@ -18,31 +18,42 @@ class Spike2ArduinoTriggerProcessor(Processor):
         Args:
             neuron_idx (_type_): index of the neuron in the spike train to use as trigger
             delay (float, optional): delay bettween the on and off of the TTL pulse in second. Defaults to 0.001.
+            max_frame_to_skip(int,optional): max length of spiktrain above witch the spike event will be omit for performance reason. Defaults to 100
         """
         assert len(neuron_idx) == len(digital_port), "The number of neuron and digital port must be the same"
         self.neuron_idx = neuron_idx
         self.digital_port = digital_port
+        self.max_frame_to_skip = max_frame_to_skip
     
     def process(self, message: SpikeTrainMessage) -> SpikeTrainMessage:
         if isinstance(message, SpikeTrainMessage):
             spiketrain = message.data
-            port_list = []
 
-            for i,idx in enumerate(self.neuron_idx):
-                if idx < spiketrain.shape[0]: # do some safety check to make sure index is valid
-                    if sum(spiketrain[idx,:]) > 0:
-                        # collect the port to trigger
-                        port_list.append(self.digital_port[i])
+            if max(self.neuron_idx) >= spiketrain.shape[0]:
+                self.log(logging.ERROR, "Neuron index is larger than spiketrain size")
+                return 
+                
+            if spiketrain.shape[1]<self.max_frame_to_skip:
+                msg_list = []
+                for i in range(spiketrain.shape[1]):
+                    #send pulse for every bin in the spiketrain
+                    port_list = []
+                    for idx, j in enumerate(self.neuron_idx):
+                        if spiketrain[j,i] >0: # check to see if there is any spikes
+                            port_list.append(self.digital_port[idx]) # collect the port to trigger
+                    
+                    if len(port_list)>0:
+                        msg_list.append(ArduinoTriggerMessage(port_list))
             
-            if len(port_list)>0:
-                return ArduinoTriggerMessage(port_list)
+                if len(msg_list)>0:
+                    return msg_list
 
 if __name__ == '__main__':
     
     logging.basicConfig(level=logging.DEBUG)
 
     
-    spike_msg1 = SpikeTrainMessage(np.array([[0,1,0,0]]).T)
+    spike_msg1 = SpikeTrainMessage(np.array([[0,1,0,0], [1,0,0,0],[0,1,0,0]]).T)
     print(spike_msg1.data.shape)
     spike_msg2 = SpikeTrainMessage(np.array([[0,0,1,0]]).T)
     spike_msg3 = SpikeTrainMessage(np.array([[0,1,0,0]]).T)
@@ -54,7 +65,7 @@ if __name__ == '__main__':
         
         
         msgTimeSource = MsgTimeSource(0.001, [spike_msg1, spike_msg2, spike_msg3, spike_msg4] )
-        spike2arduino = Spike2ArduinoTriggerProcessor([1,2,6,1,2,2,1],[13,12,11,10,9,8,7])
+        spike2arduino = Spike2ArduinoTriggerProcessor([1],[13])
         arduinoSink = ArduinoTriggerSink("COM5")
         
         msgTimeSource.connect(spike2arduino)
