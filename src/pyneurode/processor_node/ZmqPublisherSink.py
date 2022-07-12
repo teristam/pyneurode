c2cd86c5711722ee02fb9120ba7382bf7c1587c0
teristam <teristam@gmail.com>
2022-07-12 23:05:48 +0100

    zmq_node:  add zmq publisher and subscriber

---
+++ src/pyneurode/processor_node/ZmqPublisherSink.py
@@ -0,0 +1,54 @@
+import json
+import logging
+from typing import Any, Optional
+
+import zmq
+from matplotlib.font_manager import json_dump
+from pyneurode.processor_node.Message import Message
+from pyneurode.processor_node.Processor import MsgTimeSource, Sink
+from pyneurode.processor_node.ProcessorContext import ProcessorContext
+from pyneurode.processor_node.ZmqSubscriberSource import ZmqSubscriberSource
+
+
+class ZmqMessage(Message):
+    type = 'zmq_message'
+    def __init__(self, data: Any, timestamp: Optional[float] = None):
+        super().__init__(type, data, timestamp)
+
+class ZmqPublisherSink(Sink):
+    
+    def __init__(self, addr="tcp://*:5554"):
+        super().__init__()
+        self.addr = addr
+        
+    def startup(self):
+        super().startup()
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PUB)
+        self.socket.bind(self.addr)
+        self.log(logging.INFO,self.addr)
+
+    def process(self, message: Message):
+    
+        if type(message) is ZmqMessage:
+            self.socket.send_string(json.dumps(message.data))
+            
+
+
+if __name__ == '__main__':
+    
+    msg = [ZmqMessage({
+        'number': 1.34,
+        'text': 'this is a text'
+    })]
+    
+    with ProcessorContext() as ctx:
+        
+        
+        source = MsgTimeSource(1, msg)
+        zmqSink = ZmqPublisherSink()
+        zmqSource = ZmqSubscriberSource(interval=0)
+
+        source.connect(zmqSink)
+
+        ctx.start()
---
+++ src/pyneurode/processor_node/ZmqSubscriberSource.py
@@ -0,0 +1,37 @@
+
+import logging
+from pyneurode.processor_node.Processor import Source, TimeSource
+import zmq
+import json
+
+class ZmqSubscriberSource(TimeSource):
+    
+    def __init__(self, interval=0.001, addr="tcp://localhost:5554"):
+        super().__init__(interval=interval)
+        self.addr = addr
+        
+    def startup(self):
+        self.context = zmq.Context()
+        self.poller = zmq.Poller()
+
+        self.socket = self.context.socket(zmq.SUB) 
+        self.socket.connect(self.addr)
+
+        self.socket.setsockopt(zmq.SUBSCRIBE, b'')
+
+
+        self.poller.register(self.socket, zmq.POLLIN)
+        
+    def process(self):
+        socks = dict(self.poller.poll(1))
+        # check if there is any message in queues
+        
+        if self.socket in socks:
+            try:
+                message = self.socket.recv(zmq.NOBLOCK)
+            except zmq.ZMQError as err:
+                self.log(logging.ERROR, f"Got error {err}")
+                
+            if message:
+                result = json.loads(message.decode('utf-8'))
+                self.log(logging.DEBUG, f'Recevied {result}')
