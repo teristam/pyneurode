f7a503f85b9c0e6f625ce6d8962abbbd1c8026b7
teristam <teristam@gmail.com>
2023-02-28 23:20:36 +0000

    [bugfix] fix the resampling bug [feature] add in multiple way of resampling

--- src/pyneurode/processor_node/AnalogSignalMerger.py
+++ src/pyneurode/processor_node/AnalogSignalMerger.py
@@ -23,77 +23,62 @@ class AnalogSignalMerger(BatchProcessor):
     The data of the input message must be a numpy array                                              
     '''
     
-    def __init__(self, message_list:List[Message], interval:float=0.001, internal_buffer_size:int=10000,  verbose:bool=True):
+    def __init__(self, message_list:List[Message], interval:float=0.001, internal_buffer_size:int=10000,
+                 verbose:bool=True, resample_method='resample_poly'):
+        # resample method can be resample, resamply_poly, or upfirdn
         super().__init__(interval, internal_buffer_size, verbose)
-        self.message_type_list = {m:[] for m in message_list }
-        self.data_list = [[] for _ in range(len(self.message_type_list))]
+        self.message_type_list = message_list # store the type of the message that we can receive
+        self.message_type_dict = {msg:[] for msg in message_list} # dictionary of list that store the incoming messages
+        self.data_list = [[] for _ in range(len(self.message_type_list))] # data from the incoming messages, in the order of the message_type_list
+        self.resample_method = resample_method
     
     def startup(self):
         super().startup()
-        # self.buffer_length = 100*60*5
-        # self.agg_buffer = None
-        # self.adc_buffer = None
-        # self.spike_buffer = None
-        # self.readHead = 0
-        
-    # def reset_buffer(self, adc_channels:int=None, spike_train_channels:int=None):
-    #     # reset the internal buffer, usually used when the input shape has changed 
-        
-    #     if self.adc_buffer is not None:
-    #         if adc_channels is not None:
-    #             self.adc_buffer = RingBuffer((self.buffer_length, adc_channels))
-    #         else:
-    #             self.adc_buffer = RingBuffer((self.buffer_length, self.adc_buffer.buffer.shape[1]))
-        
-    #     if self.spike_buffer is not None:
-    #         if spike_train_channels is not None and self.spike_buffer is not None:
-    #             self.spike_buffer = RingBuffer((self.buffer_length, spike_train_channels))
-    #         else:
-    #             self.spike_buffer = RingBuffer((self.buffer_length, self.spike_buffer.buffer.shape[1]))
-            
-    #     self.readHead = 0
 
     def process(self,data):
         # read from two sources of data
 
         # prcoess each message
         for d in data:
-            if type(d) in self.message_type_list:
-                self.message_type_list[type(d)].append(d)
+            if type(d) in self.message_type_dict:
+                self.message_type_dict[type(d)].append(d)
+        
+        
     
         # At each analysis interval, oversample every message receive so that they are of the same length
         
         # First combine all data of the same type
-        for i, msg_list in enumerate(self.message_type_list.values()):
-            if len(msg_list) > 0:
-                d = [msg.data for msg in msg_list]
+        for i, msg_type in enumerate(self.message_type_list):
+            if len(self.message_type_dict[msg_type]) > 0:
+                d = [msg.data for msg in self.message_type_dict[msg_type]]
                 self.data_list[i] = np.concatenate(d)
+                print(f'data_list {i} {self.data_list[i].shape}')
         
         # oversample every message receive so that they are of the same length
         max_len = max([x.shape[0] for x in self.data_list])
-        print([x.shape[0] for x in self.data_list])
         
         for i in range(len(self.data_list)):
             if len(self.data_list[i]) < max_len:
-                # self.data_list[i] = signal.resample_poly(self.data_list[i], max_len, len(self.data_list[i]))
-                # self.data_list[i] = signal.resample(self.data_list[i], max_len)
-                # self.data_list[i] = signal.upfirdn([1,1,1,1,1], self.data_list[i], max_len, len(self.data_list[i]))
-
-                self.data_list[i] = signal.upfirdn([1,1,1], self.data_list[i], max_len, self.data_list[i].shape[0])
-                print('before', self.data_list[i].shape)
-                if self.data_list[i].shape[0] < max_len:
-                    self.data_list[i] = np.pad(self.data_list[i], ((0, max_len-self.data_list[i].shape[0]),(0,0)), mode='maximum')
-                    print('after', self.data_list[i].shape)
+                if self.resample_method == 'resample_poly':
+                    self.data_list[i] = signal.resample_poly(self.data_list[i], max_len, len(self.data_list[i]), axis=0, padtype='line')
+                elif self.resample_method =='resample':
+                    self.data_list[i] = signal.resample(self.data_list[i], max_len, axis=0)
+                else:
+                    h = np.ones((max_len,))
+                    self.data_list[i] = signal.upfirdn(h, self.data_list[i], max_len, self.data_list[i].shape[0],axis=0)
+                    if self.data_list[i].shape[0] < max_len:
+                        self.data_list[i] = np.pad(self.data_list[i], ((0, max_len-self.data_list[i].shape[0]),(0,0)), mode='maximum')
                 
         # make sure data are now of the same length, and the concatenate them together 
         #TODO work on the case when one of the data is missing
         sample_lens = set([len(d) for d in self.data_list])
         assert len(sample_lens) == 1, f'Error: data not of the same length {sample_lens}'      
         data_concat = np.hstack(self.data_list)
+        print(f'data_concat: {data_concat.shape}')
         
         # clear the current data
-        for k in self.message_type_list.keys():
-            self.message_type_list[k] = []
+        for k in self.message_type_dict.keys():
+            self.message_type_dict[k] = []
 
         return Message('sync_data', data_concat)
         
@@ -146,15 +131,13 @@ if __name__ == '__main__':
     with ProcessorContext() as ctx:
     
         # simulate change of message data shape
-        sine1 = SineTimeSource(0.02, 10, 2, msg_type='message1')
-        sine2 = SineTimeSource(0.1, 5, 3, msg_type='message2')
-        merger = AnalogSignalMerger( [Message1, Message2], interval=0.5)
+        sine1 = SineTimeSource(0.25, 10, 2, msg_type='message1', sampling_frequency=50)
+        sine2 = SineTimeSource(0.5, 5, 3, msg_type='message2', sampling_frequency=100)
+        merger = AnalogSignalMerger( [Message1, Message2], interval=1)
         
         gui = GUIProcessor()
         visualizer = AnalogVisualizer('Sync data')
         gui.register_visualizer(visualizer,['sync_data'])
-        
-        
         
         sink = EchoSink()
         
@@ -162,3 +145,4 @@ if __name__ == '__main__':
         sine2.connect(merger)
         merger.connect(sink)
         merger.connect(gui)
+        
