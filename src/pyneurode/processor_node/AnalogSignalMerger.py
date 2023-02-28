32ce223033c2720191902e99ce2036f9f6401838
teristam <teristam@gmail.com>
2023-02-28 00:25:56 +0000

    [node] add testing script to AnalogSignalMerger

--- src/pyneurode/processor_node/AnalogSignalMerger.py
+++ src/pyneurode/processor_node/AnalogSignalMerger.py
@@ -30,28 +30,28 @@ class AnalogSignalMerger(BatchProcessor):
     
     def startup(self):
         super().startup()
-        self.buffer_length = 100*60*5
-        self.agg_buffer = None
-        self.adc_buffer = None
-        self.spike_buffer = None
-        self.readHead = 0
+        # self.buffer_length = 100*60*5
+        # self.agg_buffer = None
+        # self.adc_buffer = None
+        # self.spike_buffer = None
+        # self.readHead = 0
         
-    def reset_buffer(self, adc_channels:int=None, spike_train_channels:int=None):
-        # reset the internal buffer, usually used when the input shape has changed 
+    # def reset_buffer(self, adc_channels:int=None, spike_train_channels:int=None):
+    #     # reset the internal buffer, usually used when the input shape has changed 
         
-        if self.adc_buffer is not None:
-            if adc_channels is not None:
-                self.adc_buffer = RingBuffer((self.buffer_length, adc_channels))
-            else:
-                self.adc_buffer = RingBuffer((self.buffer_length, self.adc_buffer.buffer.shape[1]))
+    #     if self.adc_buffer is not None:
+    #         if adc_channels is not None:
+    #             self.adc_buffer = RingBuffer((self.buffer_length, adc_channels))
+    #         else:
+    #             self.adc_buffer = RingBuffer((self.buffer_length, self.adc_buffer.buffer.shape[1]))
         
-        if self.spike_buffer is not None:
-            if spike_train_channels is not None and self.spike_buffer is not None:
-                self.spike_buffer = RingBuffer((self.buffer_length, spike_train_channels))
-            else:
-                self.spike_buffer = RingBuffer((self.buffer_length, self.spike_buffer.buffer.shape[1]))
+    #     if self.spike_buffer is not None:
+    #         if spike_train_channels is not None and self.spike_buffer is not None:
+    #             self.spike_buffer = RingBuffer((self.buffer_length, spike_train_channels))
+    #         else:
+    #             self.spike_buffer = RingBuffer((self.buffer_length, self.spike_buffer.buffer.shape[1]))
             
-        self.readHead = 0
+    #     self.readHead = 0
 
     def process(self,data):
         # read from two sources of data
@@ -70,16 +70,25 @@ class AnalogSignalMerger(BatchProcessor):
                 self.data_list[i] = np.concatenate(d)
         
         # oversample every message receive so that they are of the same length
-        max_len = max([len(x) for x in self.data_list])
+        max_len = max([x.shape[0] for x in self.data_list])
+        print([x.shape[0] for x in self.data_list])
         
         for i in range(len(self.data_list)):
             if len(self.data_list[i]) < max_len:
-                self.data_list[i] = signal.resample(self.data_list[i], max_len)
+                # self.data_list[i] = signal.resample_poly(self.data_list[i], max_len, len(self.data_list[i]))
+                # self.data_list[i] = signal.resample(self.data_list[i], max_len)
+                # self.data_list[i] = signal.upfirdn([1,1,1,1,1], self.data_list[i], max_len, len(self.data_list[i]))
+
+                self.data_list[i] = signal.upfirdn([1,1,1], self.data_list[i], max_len, self.data_list[i].shape[0])
+                print('before', self.data_list[i].shape)
+                if self.data_list[i].shape[0] < max_len:
+                    self.data_list[i] = np.pad(self.data_list[i], ((0, max_len-self.data_list[i].shape[0]),(0,0)), mode='maximum')
+                    print('after', self.data_list[i].shape)
                 
         # make sure data are now of the same length, and the concatenate them together 
         #TODO work on the case when one of the data is missing
-
-        assert len(set([len(d) for d in self.data_list])) == 1, 'Error: data not of the same length'      
+        sample_lens = set([len(d) for d in self.data_list])
+        assert len(sample_lens) == 1, f'Error: data not of the same length {sample_lens}'      
         data_concat = np.hstack(self.data_list)
         
         # clear the current data
@@ -88,43 +97,18 @@ class AnalogSignalMerger(BatchProcessor):
 
         return Message('sync_data', data_concat)
         
-        # if adc_data is not None: 
+        # if data_concat is not None: 
         #     if self.adc_buffer is None:
-        #         self.adc_buffer = RingBuffer((self.buffer_length,adc_data.shape[1]))
-        #         self.adc_buffer.write(adc_data) #format should be in time x channel
+        #         self.adc_buffer = RingBuffer((self.buffer_length,data_concat.shape[1]))
+        #         self.adc_buffer.write(data_concat) #format should be in time x channel
         #     else:
         #         try:
-        #             self.adc_buffer.write(adc_data)
+        #             self.adc_buffer.write(data_concat)
         #         except ValueError:
         #             self.log(logging.DEBUG, 'Data shape has changed. Initializing a new buffer')
-        #             self.reset_buffer(adc_channels=adc_data.shape[1])
-        #             self.adc_buffer.write(adc_data)
-            
-        # if len(spike_data_list)>0:
-        #     # self.log(logging.DEBUG, f'Concatenating {len(spike_data_list)} {spike_data_list[0].shape}')
-        #     # check if the spike data match
-        #     try:
-        #         spike_train = np.concatenate(spike_data_list,axis=0)
-        #         # self.log(logging.DEBUG, spike_train.shape)
-        #     except ValueError:
-        #         self.log(logging.DEBUG, 'Array mismatch, skipping')
-                
- 
-        # # Write to internal buffer
-        # if spike_train is not None:
-        #     # self.log(logging.DEBUG, f'spike_train shape: {spike_train.shape}')
-        #     if self.spike_buffer is None:
-        #          # create the a new buffer
-        #         self.spike_buffer = RingBuffer((self.buffer_length, spike_train.shape[1]))
-
-        #     try:
-        #         self.spike_buffer.write(spike_train)
-        #     except ValueError:
-        #         # spike train shape has changed, properly the data is resorted, discard the current content
-        #         self.log(logging.DEBUG, f'Data shape has changed. Initializing a new buffer : {spike_train.shape} old buffer shape: {self.spike_buffer.buffer.shape}')
-        #         self.reset_buffer(spike_train_channels=spike_train.shape[1])
-        #         self.spike_buffer.write(spike_train) # time x channel
-        
+        #             self.reset_buffer(adc_channels=data_concat.shape[1])
+        #             self.adc_buffer.write(data_concat)
+                    
         # msg_data_types = [d.type for d in data]
         # # self.log(logging.DEBUG, f'Processing data: {Counter(msg_data_types)}')
 
@@ -162,9 +146,9 @@ if __name__ == '__main__':
     with ProcessorContext() as ctx:
     
         # simulate change of message data shape
-        sine1 = SineTimeSource(0.05, 10, 2, msg_type='message1')
+        sine1 = SineTimeSource(0.02, 10, 2, msg_type='message1')
         sine2 = SineTimeSource(0.1, 5, 3, msg_type='message2')
-        merger = AnalogSignalMerger( [Message1, Message2], interval=0.2)
+        merger = AnalogSignalMerger( [Message1, Message2], interval=0.5)
         
         gui = GUIProcessor()
         visualizer = AnalogVisualizer('Sync data')
