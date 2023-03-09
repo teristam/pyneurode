f053c914f7355e756e41d7f7dacb89b10311ca11
teristam <teristam@gmail.com>
2023-03-09 00:19:26 +0000

    [signal merger] simplify logic of signal merge to use Ring Buffer instead To avoid upsampling bug when messages come in at irregular interval

--- src/pyneurode/processor_node/AnalogSignalMerger.py
+++ src/pyneurode/processor_node/AnalogSignalMerger.py
@@ -20,17 +20,24 @@ from scipy import signal
 class AnalogSignalMerger(BatchProcessor):
     '''
     Synchronize two source of analoge signal. The lower-sampled signal will be upsampled. The two input message must of different type 
-    The data of the input message must be a numpy array                                              
+    The data of the input message must be a numpy array  
+
+    This class synchronizes two sources of an analog signal. The lower-sampled signal will be upsampled to match the other source’s data rate.
     '''
+
     
-    def __init__(self, message_list:List[Message], interval:float=0.001, internal_buffer_size:int=10000,
-                 verbose:bool=True, resample_method='resample_poly'):
-        # resample method can be resample, resamply_poly, or upfirdn
+    def __init__(self, message_type_info:Dict[Message, Tuple[int,int]],
+                 interval:float=0.001, internal_buffer_size:int=10000,
+                 verbose:bool=True, resample_method:str='resample_poly'):
+        # format of message_list: Dict(message_type, Tuple(up, down)
+        # resample method can be resamply_poly, or upfirdn
         super().__init__(interval, internal_buffer_size, verbose)
-        self.message_type_list = message_list # store the type of the message that we can receive
-        self.message_type_dict = {msg:[] for msg in message_list} # dictionary of list that store the incoming messages
-        self.data_list = [[] for _ in range(len(self.message_type_list))] # data from the incoming messages, in the order of the message_type_list
+        self.message_type_info = message_type_info # store the type of the message that we can receive
+        self.message_type_dict = {k:[] for k in message_type_info.keys()} # dictionary of list that store the incoming messages
+        self.data_list = {k:None for k in self.message_type_info.keys()} #store the ringbuffer for each data type
         self.resample_method = resample_method
+        self.internal_buffer_size = internal_buffer_size
+        self.read_head = 0
     
     def startup(self):
         super().startup()
@@ -46,47 +53,51 @@ class AnalogSignalMerger(BatchProcessor):
         
         # At each analysis interval, oversample every message receive so that they are of the same length
         
-        # First combine all data of the same type
-        data_incomplete = False
-        for i, msg_type in enumerate(self.message_type_list):
-            if len(self.message_type_dict[msg_type]) > 0:
-                d = [msg.data for msg in self.message_type_dict[msg_type]]
-                self.data_list[i] = np.concatenate(d)
-                print(f'data_list {i} {self.data_list[i].shape}')
-                self.message_type_dict[msg_type] = [] # clear queue after processing
-            else:
-                # print(f'Waiting for {msg_type} to arrive')
-                data_incomplete = True
-        
-        #TODO seems to have bug messing up the signal frequency when only one message type arrive
-        # now the resampling depends on how fast the data comes in, which may be affected by other factors
-        # this will lead to distortion of the signal after resampling
-        if data_incomplete: # early exist if at least one message type has not arrive, only proceed when all message types have arrived
-            return 
+        for msg_type, msg_list in self.message_type_dict.items():
+            if len(msg_list)>0:
+                # First combine all data of the same type
 
-        
-        # oversample every message receive so that they are of the same length
-        max_len = max([x.shape[0] for x in self.data_list])
-        
-        for i in range(len(self.data_list)):
-            if len(self.data_list[i]) < max_len:
-                if self.resample_method == 'resample_poly':
-                    self.data_list[i] = signal.resample_poly(self.data_list[i], max_len, len(self.data_list[i]), axis=0, padtype='line')
-                elif self.resample_method =='resample':
-                    self.data_list[i] = signal.resample(self.data_list[i], max_len, axis=0)
-                else:
-                    h = np.ones((max_len,))
-                    self.data_list[i] = signal.upfirdn(h, self.data_list[i], max_len, self.data_list[i].shape[0],axis=0)
-                    if self.data_list[i].shape[0] < max_len:
-                        self.data_list[i] = np.pad(self.data_list[i], ((0, max_len-self.data_list[i].shape[0]),(0,0)), mode='maximum')
+                d = [msg.data for msg in msg_list]
+                d = np.concatenate(d)
                 
-        # make sure data are now of the same length, and the concatenate them together 
-        sample_lens = set([len(d) for d in self.data_list])
-        assert len(sample_lens) == 1, f'Error: data not of the same length {sample_lens}'      
-        data_concat = np.hstack(self.data_list)
-        # print(f'data_concat: {data_concat.shape}')
+                # upsample
+                up, down  = self.message_type_info[msg_type]
+                if self.resample_method == 'resample_poly':
+                    d = signal.resample_poly(d, up, down, axis=0, padtype='line')
+                else:
+                    # sample-and-hold
+                    h = np.ones(up)
+                    d = signal.upfirdn(h, d, up, down,axis=0)
+        
+                
+                # print(f'{msg_type} : {d.shape=}')
+                
+                # write to ring buffer
+                if self.data_list[msg_type] is None:
+                    self.data_list[msg_type] = RingBuffer((self.internal_buffer_size, d.shape[1]))
+                
+                self.data_list[msg_type].write(d)
+                
+                self.message_type_dict[msg_type] = [] # clear queue after processing
 
-        return Message('sync_data', data_concat)
+        # make sure all ring buffer is initiated before procedding
+        if any([buffer is None for buffer in self.data_list.values()]):
+            return
+
+        # Read from the ring buffers
+        curReadHead = min([buffer.absWriteHead for buffer in self.data_list.values()])
+        data2read = curReadHead - self.read_head 
+
+        if data2read >0:
+
+            spike_train_data = [buffer.readContinous(data2read) for buffer in self.data_list.values()]
+
+            msg_data = np.hstack(spike_train_data)
+
+            self.read_head += data2read
+
+            return Message('sync_data', msg_data)
+
         
 
 if __name__ == '__main__':
@@ -97,7 +108,7 @@ if __name__ == '__main__':
         # simulate change of message data shape
         sine1 = SineTimeSource(0.1, 10, 2, msg_type='message1', sampling_frequency=50)
         sine2 = SineTimeSource(0.5, 5, 3, msg_type='message2', sampling_frequency=100)
-        merger = AnalogSignalMerger( [Message1, Message2], interval=0.6)
+        merger = AnalogSignalMerger( {Message1:(2,1), Message2:(1,1)}, interval=0.4)
         
         gui = GUIProcessor()
         visualizer = AnalogVisualizer('Sync data', scale=5)
